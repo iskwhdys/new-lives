0f0d99db62c719719da9bdae083346c01fd7b261
iskwhdys <iskwhdys@gmail.com>
2020-10-26 22:15:19 +0900

    サブチャンネル・コラボチャンネルの情報追加

---
+++ db/14_data_liver_tag.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.liver_tag (id,"key",value) VALUES 
+('Devidevi_Devil','youtube','UCP19rQ5V-46B-6ZeLDJqp5w')
+,('Kanae','youtube','UCz6vnIbgiqFT9xUcD6Bp65Q')
+,('Ryushen','youtube','UCTi_rzf5QIkXjhJjkbcAdTg')
+,('Suzuya_Aki','youtube','UCt9qik4Z-_J-rj3bKKQCeHg')
+,('Ratna_Petit','youtube','UCpfjQCCavrO-rnKaAaIF9dg')
+,('Kuzuha','youtube','UCz6vnIbgiqFT9xUcD6Bp65Q')
+,('Masaru_Suzuki','youtube','UCaF-mODqAziHivqg0Q61XKQ')
+;
